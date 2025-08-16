64ada6598b23c82516ccff21ca4f1f51d42fefcb
Dimah <Dim.Althobaiti@gmail.com>
2025-08-16 21:11:10 +0300

    Update settings.py

--- RobotArm/RobotArm/settings.py
+++ RobotArm/RobotArm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2te-rzf39$^7b223k(zshhhx5rb#nk%flztgnhnhmpd#vpb@^1'
+SECRET_KEY = '-------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
